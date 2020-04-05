c6e1a8997690d590b4f4697ced13e4094a5fff0e
szattila98 <clownface68@gmail.com>
2020-04-05 14:21:40 +0200

    Added multiple extension arguments support.

--- filemover.py
+++ filemover.py
@@ -4,19 +4,33 @@ fullpath = os.path.join
 
 CURRENT_DIR = os.getcwd()
 RESULT_FOLDER_NAME = 'results'
-EXTENSION = '.' + sys.argv[1] if len(sys.argv) > 1 and sys.argv[1][0] != '.' else sys.argv[1] if len(sys.argv) > 1 else ''
-RESULT_FOLDER_PATH = fullpath(CURRENT_DIR, RESULT_FOLDER_NAME, EXTENSION)
-SUBFOLDER_PATHS = [f.path for f in os.scandir(CURRENT_DIR) if f.is_dir() and f.name != RESULT_FOLDER_NAME]
+EXTENSIONS = ['.' + arg if len(sys.argv) > 1 and arg[0] != '.' else arg if len(sys.argv) > 1 else '' for arg in sys.argv[1:]]
+RESULT_FOLDER_PATHS = [fullpath(CURRENT_DIR, RESULT_FOLDER_NAME, ext) for ext in EXTENSIONS]
+SUBFOLDER_PATHS = [f.path for f in os.scandir(CURRENT_DIR) if f.is_dir() and f.name != RESULT_FOLDER_NAME and f.name != '.git']
 
-print("\nfilemover running...")
-os.makedirs(RESULT_FOLDER_PATH, exist_ok=True)
-files_to_move = []
-for path in SUBFOLDER_PATHS:
-    for (dir, dirs, files) in os.walk(path):
-        for file in files:
-            if file.endswith(EXTENSION):
-                files_to_move.append(fullpath(dir, file))
-print(files_to_move)
-print(RESULT_FOLDER_PATH)
-for file in files_to_move:
-    shutil.move(file, fullpath(RESULT_FOLDER_PATH, os.path.basename(file)))
+if EXTENSIONS:
+    print("\nfilemover running...")
+
+    files_to_move = []
+    for ext in EXTENSIONS:
+        for path in SUBFOLDER_PATHS:
+            for (dir, dirs, files) in os.walk(path):
+                for file in files:
+                    if file.endswith(ext):
+                        files_to_move.append(fullpath(dir, file))
+
+    if not files_to_move:
+        print('There is no file with this extension within any of the sub directories!\n')
+        exit()
+
+    for path in RESULT_FOLDER_PATHS:
+        for file in files_to_move:
+            if file.endswith(os.path.basename(path)):
+                newFile = fullpath(path)
+                os.makedirs(newFile, exist_ok=True)
+                shutil.move(file, newFile)
+                print(f'{file} -> {newFile}')
+                    
+    print(f'Moved {len(files_to_move)} files!\n')
+else:
+    print('No extension arguments specified!')
